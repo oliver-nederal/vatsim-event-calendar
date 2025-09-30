af9f6709dcc2b612d12f2315582f5e6a2922ce85
Oliver Nederal <oliver.nederal@gmail.com>
2025-09-30 16:44:41 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
         <header className="bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-700 px-4 py-2 w-full flex">
           <div className="w-full flex justify-between items-center">
             <h1 className="text-2xl font-bold text-gray-900 dark:text-white">
-              VATSIM Events Calendar
+              Events Calendar
             </h1>
             <div className="flex items-center space-x-4">
               <ThemeSelector />
