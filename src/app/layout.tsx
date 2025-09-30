1c3e99fa4164d0f299f05832d85e1d877a306763
Oliver Nederal <oliver.nederal@gmail.com>
2025-09-30 16:06:44 +0200

    add preview image, update types

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-// @ts-expect-error - globals.css does not have types/exists
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 
